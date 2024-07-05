fedba5601f710454593c7bf0242e5dbe5d1a2910
Aurelius Ivan Wijaya <aureliusivanwijaya@gmail.com>
2024-07-05 14:00:14 +0700

    remove gradio

--- worker.js
+++ worker.js
@@ -1,15 +1,5 @@
-// import {Client} from "@gradio/client/dist/client";
-
-import {Client} from "@gradio/client";
-
-
 export default {
   async fetch(request, env, ctx) {
-    const client = await Client.connect("aureliusivan/DiffuseCraft");
-    const result = await client.predict("/update_task_options", {
-      model_name: "stabilityai/stable-diffusion-xl-base-1.0", task_name: "txt2img",
-    });
-
-    return new Response(JSON.stringify(result));
+    return new Response(JSON.stringify("Hello World!"));
   },
 };
